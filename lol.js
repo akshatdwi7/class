c0ef152f95bf3703c0a272fc607ad3fc9596d8c0
Akshat Dwivedi <akshatdwi755@gmail.com>
2024-09-29 23:58:30 +0530

    j
    n

--- lol.js
+++ lol.js
@@ -1,2 +1,9 @@
 let bro = Math.random(8)
 console.log(bro+3)
+ const w  = 5 
+ var i = 9
+ console.log(w+i)
+ function works(){
+    console.log("works")
+ }
+ works()
--- work.tsx
+++ work.tsx
@@ -1,2 +1,2 @@
 import ration data 
-f// the new purplose of the develiopment is sick 
+f// the new purplose of the develiopment is sick    
