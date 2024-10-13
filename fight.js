634ed5130251cafac5878485839f42effb359c71
Akshat Dwivedi <akshatdwi755@gmail.com>
2024-10-13 16:19:49 +0530

    push game data

--- fight.js
+++ fight.js
@@ -7,8 +7,7 @@ fightButton.addEventListener("click", function() {
     // Challenge:
     // When the user clicks on the "Pick Fighters" button, pick two random 
     // emoji fighters and display them as i.e. "🦀 vs 🐢" in the "stage" <div>.
-    let randomFight = Math.floor(Math.random() *fighters.length) 
-    console.log (fighters[randomFight])
-
-
+    let randomIndexOne = Math.floor( Math.random() * fighters.length )
+    let randomIndexTwo = Math.floor( Math.random() * fighters.length )
+    stageEl.textContent = fighters[randomIndexOne] + " vs " + fighters[randomIndexTwo]
 })
